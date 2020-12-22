7a7caeaf543e2ca29a452073d3e515ccbaad5e68
karthik12-hub <74128200+karthik12-hub@users.noreply.github.com>
2020-12-22 12:40:02 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box{
+constructor(x,y,width,height){
+    var options={
+   isStatic:true
+    }
+    this.body=Bodies.rectangle(x,y,width,height,options)
+    this.width=width
+    this.height=height
+}
+display(){
+    var pos=this.body.position
+push();
+rectMode(CENTER)
+translate(this.body.position.x,this.body.position,y)
+rect(this.body.position.x,this.body.position.y,this.width,this.height)
+pop();
+}
+}
